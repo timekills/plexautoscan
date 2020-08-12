81ae3d52ce073c9b1ca7bd0d249f677c2f1dd6c0
doob187 <60312740+doob187@users.noreply.github.com>
2020-08-12 18:38:45 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -14,7 +14,7 @@ function clone() {
     sudo rm -rf /opt/plexguide/menu/pgscan
     sudo git clone --quiet https://github.com/doob187/plexautoscan.git /opt/plexguide/menu/pgscan
     sudo chown -cR 1000:1000 /opt/plexguide/menu/
-    sudo chmod -cR 1000:1000 /opt/plexguide/menu/
+    sudo chmod -cR 755 /opt/plexguide/menu/
     sudo bash /opt/plexguide/menu/pgscan.sh
 }
 sudocheck
