acd3ea0c6f7bfce4cbdedc9081c2592a92dac60f
doob187 <60312740+doob187@users.noreply.github.com>
2020-08-12 18:35:20 +0200

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+function sudocheck () {
+  if [[ $EUID -ne 0 ]]; then
+    tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  You Must Execute as a SUDO USER (with sudo) or as ROOT!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+    exit 0
+  fi
+}
+
+function clone() {
+    sudo rm -rf /opt/plexguide/menu/pgscan
+    sudo git clone --quiet https://github.com/doob187/plexautoscan.git /opt/plexguide/menu/pgscan
+    sudo chown -cR 1000:1000 /opt/plexguide/menu/
+    sudo chmod -cR 1000:1000 /opt/plexguide/menu/
+    sudo bash /opt/plexguide/menu/pgscan.sh
+}
+sudocheck
+clone
