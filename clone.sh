33efd5933b924d55d42d8e4a57c5d9e92cfd0f90
Timekills <timekills@timekills.org>
2020-12-03 23:43:31 +0100

    changed to timekills repo

--- clone.sh
+++ clone.sh
@@ -12,7 +12,7 @@ EOF
 
 function clone() {
     sudo rm -rf /opt/plexguide/menu/pgscan
-    sudo git clone --depth 1 --single-branch --branch master https://github.com/doob187/plexautoscan /opt/plexguide/menu/pgscan
+    sudo git clone --depth 1 --single-branch --branch master https://github.com/timekills/plexautoscan /opt/plexguide/menu/pgscan
     sudo chown -cR 1000:1000 /opt/plexguide/menu/
     sudo chmod -cR 755 /opt/plexguide/menu/
     sudo bash /opt/plexguide/menu/pgscan/pgscan.sh
